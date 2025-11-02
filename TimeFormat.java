65a10504d0f260bc2aec8a90392dc0e6b9468fd4
Ohad Gueta <ohad.gueta@post.runi.ac.il>
2025-11-02 17:33:30 +0200

    Completed assignment

--- TimeFormat.java
+++ TimeFormat.java
@@ -12,6 +12,31 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+        
+		String time = "AM"; 
+
+		if(hours > 12)
+		{
+			time = "PM";
+			hours = hours - 12;
+
+		}
+		if(hours == 0)
+		{
+			time = "AM";
+			if(minutes < 10)
+		{
+			System.out.println("12" + ":0" + minutes + " " + time);
+		}
+		}
+		else if(minutes < 10)
+		{
+			System.out.println(hours + ":0" + minutes + " " + time);
+		}
+		else
+		{
+			System.out.println(hours + ":" + minutes + " " + time);
+		}
+
 	}
 }
